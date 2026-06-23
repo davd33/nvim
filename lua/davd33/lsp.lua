c40f5e51753b164a5daf7082d23a64c47296e48e
David Rueda <davd33@gmail.com>
2026-06-23 23:58:16 +0200

    rust type hints

--- lua/davd33/lsp.lua
+++ lua/davd33/lsp.lua
@@ -8,3 +8,11 @@ vim.lsp.config("basedpyright", {
     capabilities = capabilities,
 })
 
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+})
+
--- lua/davd33/remap.lua
+++ lua/davd33/remap.lua
@@ -4,6 +4,10 @@ vim.g.maplocalleader = "\\"
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("v", "<leader>ca", vim.lsp.buf.code_action)
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
 vim.keymap.set("n", "<leader>rf", vim.lsp.buf.rename)
 vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references)
 
+vim.keymap.set("n", "<leader>th", function()
+  vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+end)
