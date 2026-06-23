f535f72d0a9b7769bcec686d6de8ffe5b7662420
David Rueda <davd33@gmail.com>
2026-06-23 21:39:30 +0200

    color scheme

--- lua/davd33/lsp.lua
+++ lua/davd33/lsp.lua
@@ -1,6 +1,5 @@
 vim.lsp.enable('lua_ls')
 vim.lsp.enable('rust_analyzer')
---vim.lsp.enable('pylsp')
 vim.lsp.enable('basedpyright')
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -8,3 +7,4 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 vim.lsp.config("basedpyright", {
     capabilities = capabilities,
 })
+
