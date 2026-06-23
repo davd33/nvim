df2018cffce81f4ea4d25e683b0b712a85a8737f
David Rueda <davd33@gmail.com>
2026-06-23 17:20:06 +0200

    improve lsp - python

--- lua/davd33/lsp.lua
+++ lua/davd33/lsp.lua
@@ -1,2 +1,10 @@
 vim.lsp.enable('lua_ls')
 vim.lsp.enable('rust_analyzer')
+--vim.lsp.enable('pylsp')
+vim.lsp.enable('basedpyright')
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+vim.lsp.config("basedpyright", {
+    capabilities = capabilities,
+})
