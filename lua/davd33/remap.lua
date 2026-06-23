809fe00422f8c1fd63367c8c924e5cd76e4d2378
David Rueda <davd33@gmail.com>
2026-06-23 23:09:56 +0200

    remap lsp code action, goto refs, rename

--- lua/davd33/remap.lua
+++ lua/davd33/remap.lua
@@ -2,3 +2,8 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("v", "<leader>ca", vim.lsp.buf.code_action)
+vim.keymap.set("n", "<leader>rf", vim.lsp.buf.rename)
+vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references)
+
