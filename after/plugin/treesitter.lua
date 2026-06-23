5faf1f75825513c08029701cc25a7dadf45c9498
David Rueda <davd33@gmail.com>
2026-06-23 16:15:41 +0200

    trying make lsp do more

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,1 +1,24 @@
-require('nvim-treesitter').install { 'rust', 'javascript', 'html', 'lua' }
+require('nvim-treesitter').install { 'rust', 'javascript', 'html', 'lua', 'python' }
+require("nvim-treesitter").setup {
+    highlight = {
+        enable = true,
+        disable = function(lang, buf) 
+            local max_filesize = 100 * 1024
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+                return true
+            end
+        end,
+    }
+}
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'rust', 'python', 'javascript', 'lua', 'html' },
+  callback = function()
+    vim.treesitter.start()                                    -- highlighting
+    --vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'     -- folds
+    --vim.wo.foldmethod = 'expr'
+    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()" -- indentation
+  end,
+})
+
