69b7deef179b4eba8585e742811156972514bd4b
David Rueda <davd33@gmail.com>
2026-06-28 22:13:31 +0200

    super html LSP support

--- lua/davd33/lsp.lua
+++ lua/davd33/lsp.lua
@@ -9,10 +9,22 @@ vim.lsp.config("basedpyright", {
 })
 
 vim.diagnostic.config({
-  virtual_text = true,
-  signs = true,
-  underline = true,
-  update_in_insert = false,
-  severity_sort = true,
+    virtual_text = false,
+    virtual_lines = { current_line = true },
+    signs = true,
+    underline = true,
+    update_in_insert = false,
+    severity_sort = true,
 })
 
+-- HTML
+vim.api.nvim_create_autocmd("Filetype", {
+	pattern = { "html", "shtml", "htm" },
+	callback = function()
+		vim.lsp.start({
+			name = "superhtml",
+			cmd = { "superhtml", "lsp" },
+			root_dir = vim.fs.dirname(vim.fs.find({".git"}, { upward = true })[1])
+		})
+	end
+})
