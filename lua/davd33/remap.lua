f7905080eb747f99c57c8649375622c8e14d433b
David Rueda <davd33@gmail.com>
2026-06-28 22:13:45 +0200

    Oil - file explorer

--- lua/davd33/remap.lua
+++ lua/davd33/remap.lua
@@ -1,5 +1,5 @@
 -- files tree 
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Oil)
 
 -- moving selection up and down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
