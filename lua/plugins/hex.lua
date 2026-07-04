353c3b61be7698eaa0d69be90ccae366442e1771
David Rueda <davd33@gmail.com>
2026-07-04 20:01:07 +0200

    hexadecimal editing of binary files

---
+++ lua/plugins/hex.lua
@@ -0,0 +1,15 @@
+return {
+    "RaafatTurki/hex.nvim",
+    config = function ()
+        -- defaults
+        require 'hex'.setup {
+
+            -- cli command used to dump hex data
+            dump_cmd = 'xxd -g 1 -u',
+
+            -- cli command used to assemble from hex data
+            assemble_cmd = 'xxd -r',
+
+        }
+    end
+}
